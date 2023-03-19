93a16b65d4f1800a44dcb1ce1b29517493402cfc
Etienne Schmitz <contact@etienne-schmitz.com>
2023-03-19 13:38:38 +0100

    chore: Remove namespace for the library

--- src/base_station/main.cpp
+++ src/base_station/main.cpp
@@ -2,14 +2,14 @@
 #include <swo.h>
 
 #define BLINKING_RATE 500ms
-static swo::SWO swo_st;
+static SWO swo;
 
 DigitalOut led(LED1);
 
 int main() {
     while (true) {
         led = !led;
-        swo_st.println("Hello, World!");
+        swo.println("Hello, World!");
         ThisThread::sleep_for(BLINKING_RATE);
     }
 }
