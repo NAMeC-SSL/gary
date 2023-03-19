8c53fa6bb10718f31fcb5841834dc727c2bce706
Etienne Schmitz <contact@etienne-schmitz.com>
2023-03-19 13:25:50 +0100

    feat: add swo library for debugging

--- src/base_station/main.cpp
+++ src/base_station/main.cpp
@@ -1,12 +1,15 @@
 #include "mbed.h"
+#include <swo.h>
 
 #define BLINKING_RATE 500ms
+static swo::SWO swo_st;
 
 DigitalOut led(LED1);
 
 int main() {
     while (true) {
         led = !led;
+        swo_st.println("Hello, World!");
         ThisThread::sleep_for(BLINKING_RATE);
     }
 }
